a23fb8a6699fea5919d0f5538016563f798970bc
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-11-29 20:11:46 +0700

    add login page

---
+++ client/src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ui container">
+    <div class="ui middle aligned column centered grid">
+      <div class="twelve wide mobile six wide tablet four wide computer column">
+        <div class="">
+        <button class="tengah ui fluid facebook button">
+          <i class="facebook icon"></i>
+          Login with Facebook
+        </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tengah {
+    margin-top: 300px !important;
+  }
+</style>
